8ab4a90e410af257ed5adf8e1c9b3492de8f0e4b
Chris Lyon <chris@lyon.la>
2025-04-15 00:03:24 -0700

    added array list structure

---
+++ utils/array_list.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array_list.h"
+
+/**
+ * Allocate new array in array list
+ *
+ * @param lst Array list
+ * @return 0 on success, -1 on failure
+ */
+static int alloc_array(array_list* lst) {
+    void* new_array = malloc(lst->capacity * lst->value_size);
+    if (new_array == NULL) {
+        perror("alloc_array: malloc() failed");
+        return -1;
+    }
+
+    lst->array = new_array;
+
+    return 0;
+}
+
+int array_list_init(array_list* lst, const size_t value_size, const size_t capacity) {
+    lst->size = 0;
+    lst->value_size = value_size;
+    lst->capacity = capacity;
+    lst->array = NULL;
+
+    if (alloc_array(lst) != 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+static void shift_left(array_list* lst, size_t pos) {
+    --lst->size;
+
+    for (size_t i = pos; i < lst->size; ++i) {
+        lst->array[i] = lst->array[i + 1];
+    }
+}
+
+static void shift_right(array_list* lst, size_t pos) {
+    if ((pos + 1) >= lst->capacity) {
+        array_list_resize(lst, pos + 1);
+    }
+
+    for (size_t i = lst->size; i > pos; --i) {
+        lst->array[i] = lst->array[i - 1];
+    }
+}
+
+int array_list_insert_at(array_list* lst, void* value, const size_t pos) {
+    if (pos > lst->size) {
+        fprintf(stderr, "array_list_insert_at: index %zu is out of bounds %zu", pos, lst->size);
+        return -1;
+    }
+
+    if (lst->size == 0 && pos == 0) {
+        lst->array[0] = value;
+        ++lst->size;
+        return 0;
+    }
+
+    shift_right(lst, pos);
+
+    lst->array[pos] = value;
+    ++lst->size;
+
+    return 0;
+}
+
+void* array_list_get_at(const array_list* lst, const size_t pos) {
+    if (pos >= lst->size) {
+        return NULL;
+    }
+
+    return lst->array[pos];
+}
+
+void* array_list_del_at(array_list* lst, const size_t pos) {
+    if (pos >= lst->size) {
+        return NULL;
+    }
+
+    void* value = array_list_get_at(lst, pos);
+
+    shift_left(lst, pos);
+
+    return value;
+}
+
+int array_list_push_head(array_list* lst, void* value) {
+    return array_list_insert_at(lst, value, 0);
+}
+
+void* array_list_pop_head(array_list* lst) {
+    if (lst->size == 0) {
+        return NULL;
+    }
+
+    return array_list_del_at(lst, 0);
+}
+
+int array_list_push_tail(array_list* lst, void* value) {
+    return array_list_insert_at(lst, value, lst->size);
+}
+
+void* array_list_pop_tail(array_list* lst) {
+    if (lst->size == 0) {
+        return NULL;
+    }
+
+    return array_list_del_at(lst, lst->size - 1);
+}
+
+/**
+ * Copy an array
+ *
+ * @param src Source array
+ * @param dst Destination array
+ * @param length Number of items to copy
+ */
+static void copy_array(void** src, void** dst, const size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        dst[i] = src[i];
+    }
+}
+
+int array_list_resize(array_list* lst, const size_t capacity) {
+    void* old_array = lst->array;
+    const size_t old_capacity = lst->capacity;
+
+    if (capacity == lst->capacity) {
+        // ignore
+        return 0;
+    }
+
+    if (capacity < lst->capacity && capacity < lst->size) {
+        fprintf(stderr, "array_list_resize: new capacity %zu is smaller than list size %zu\n", capacity, lst->size);
+        return -1;
+    }
+
+    printf("array_list_resize: resizing from %zu to %zu\n", lst->capacity, capacity);
+    lst->capacity = capacity;
+
+    if (alloc_array(lst) == -1) {
+        lst->capacity = old_capacity;
+        return -1;
+    }
+
+    copy_array(old_array, lst->array, lst->size);
+
+    return 0;
+}
+
+int array_list_destroy(array_list* lst) {
+    if (lst->array != NULL) {
+        free(lst->array);
+        lst->array = NULL;
+    }
+
+    lst->size = 0;
+    lst->value_size = 0;
+    lst->capacity = 0;
+
+    return 0;
+}
---
+++ utils/array_list.h
@@ -0,0 +1,100 @@
+#ifndef __ARRAY_LIST_H_DEFINED__
+#define __ARRAY_LIST_H_DEFINED__
+
+typedef struct array_list {
+  size_t size;
+  size_t value_size;
+  size_t capacity;
+  void** array;
+} array_list;
+
+/**
+ * Initialize array list
+ *
+ * @param lst Empty list to initialize
+ * @param value_size Size of each value in array
+ * @param capacity Max capacity of array list (before it gets resized)
+ * @return 0 on success, -1 on failure
+ */
+int array_list_init(array_list* lst, size_t value_size, size_t capacity);
+
+/**
+ * Insert value into array list at position
+ *
+ * @param lst Array list
+ * @param value Value to insert
+ * @param pos Position to insert at
+ * @return 0 on success, -1 on failure
+ */
+int array_list_insert_at(array_list* lst, void* value, size_t pos);
+
+/**
+ * Get value at position in array list
+ *
+ * @param lst Array list
+ * @param pos Position to get item at
+ * @return Value of item (or NULL it not found)
+ */
+void* array_list_get_at(const array_list* lst, size_t pos);
+
+/**
+ * Delete value at position in array list
+ *
+ * @param lst Array list
+ * @param pos Position to delete at
+ * @return Value at position (or NULL if not found)
+ */
+void* array_list_del_at(array_list* lst, size_t pos);
+
+/**
+ * Push value to head of array list (prepend)
+ *
+ * @param lst Array list
+ * @param value Value
+ * @return 0 on success, -1 on failure
+ */
+int array_list_push_head(array_list* lst, void* value);
+
+/**
+ * Pop value from head of array list
+ *
+ * @param lst Array list
+ * @return Value that was popped (or NULL if not found)
+ */
+void* array_list_pop_head(array_list* lst);
+
+/**
+ * Push value to tail of array list (append)
+ *
+ * @param lst Array list
+ * @param value Value
+ * @return 0 on success, -1 on failure
+ */
+int array_list_push_tail(array_list* lst, void* value);
+
+/**
+ * Pop last (tail) value from array list
+ *
+ * @param lst Array list
+ * @return Value from tail (or NULL if empty)
+ */
+void* array_list_pop_tail(array_list* lst);
+
+/**
+ * Resize array list
+ *
+ * @param lst Array list
+ * @param capacity New capacity
+ * @return 0 on success, -1 on failure
+ */
+int array_list_resize(array_list* lst, size_t capacity);
+
+/**
+ * Destroy array list
+ *
+ * @param lst Array list
+ * @return 0 on success, -1 on failure
+ */
+int array_list_destroy(array_list* lst);
+
+#endif
